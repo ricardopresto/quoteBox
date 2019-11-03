18a78b6e1474f1fa53b415c4abdf7c1e73f7f80e
ricardopresto <ricardopresto5@gmail.com>
2019-11-03 19:32:16 +0100

    minor changes

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,12 +1,73 @@
 <template>
-  <div></div>
+  <div>
+    <div id="container">
+      <div id="form">
+        <div class="input">
+          <label for="username">Username:</label>
+          <input type="text" name="username" placeholder="Enter Username..." v-model="username" />
+        </div>
+        <div class="input">
+          <label for="password">Password:</label>
+          <input type="password" name="password" placeholder="Enter Password..." v-model="password" />
+        </div>
+        <div>
+          <button>Cancel</button>
+          <button>Log In</button>
+        </div>
+        <div id="messageBox">
+          <div v-show="error == true">Please enter a username and password...</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      error: false
+    };
+  },
+  methods: {}
 };
 </script>
 
 <style scoped>
+#container {
+  width: 400px;
+  height: 200px;
+  border: 1px solid grey;
+  border-radius: 8px;
+  margin: 5px;
+  background-image: url("paper.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+#form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.input {
+  margin: 8px;
+}
+input {
+  border: 1px solid grey;
+  width: 200px;
+  margin-left: 8px;
+}
+button {
+  width: 80px;
+  margin: 8px;
+}
+#messageBox {
+  height: 20px;
+}
 </style>
